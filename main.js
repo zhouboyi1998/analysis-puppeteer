ba5eb4b96c7aa00df39a128fa1092a1b84e9ae56
zhouboyi <1144188685@qq.com>
2023-09-20 23:23:54 +0800

    feat(dir): 自动创建保存结果的文件夹

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
+const fs = require('fs')
 const puppeteer = require('puppeteer')
+
+// 封装删除文件夹方法
+function empty(dir) {
+    // 判断文件夹是否存在
+    if (fs.existsSync(dir)) {
+        // 读取文件夹下的所有子节点
+        const files = fs.readdirSync(dir)
+        // 遍历子节点
+        files.forEach(file => {
+            const path = `${ dir }/${ file }`
+            const stat = fs.statSync(path)
+            if (stat.isDirectory()) {
+                // 如果节点是文件夹, 递归删除
+                empty(path)
+            } else {
+                // 如果节点是文件, 直接删除
+                fs.unlinkSync(path)
+            }
+        })
+        fs.rmdirSync(dir)
+    }
+}
+
+// 文件夹名称
+const dir = './data'
+
+// 调用方法删除文件夹
+empty(dir)
+
+// 重新创建文件夹
+fs.mkdir(dir, err => {
+    if (err) throw err
+})
 
 // 网站列表
 const websites = [
@@ -29,7 +63,7 @@ websites.forEach(website => {
         // 跳转到指定网站的页面
         await page.goto(website.url)
         // 截图保存
-        await page.screenshot({ path: './image/' + website.name + '.png' })
+        await page.screenshot({ path: dir + '/' + website.name + '.png' })
         // 关闭浏览器实例
         await browser.close()
     })()
